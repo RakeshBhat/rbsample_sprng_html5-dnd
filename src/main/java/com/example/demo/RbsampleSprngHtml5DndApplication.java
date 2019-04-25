8f571a2f5bac6cfa67b968682e1c0ef7983c826e
RakeshBhat <rakesh.bhatji@gmail.com>
2019-04-25 07:08:33 +0100

    spring boot,mvc file DnD

---
+++ src/main/java/com/example/demo/RbsampleSprngHtml5DndApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbsampleSprngHtml5DndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbsampleSprngHtml5DndApplication.class, args);
+	}
+
+}
